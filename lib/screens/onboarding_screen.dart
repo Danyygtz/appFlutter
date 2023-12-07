d7c3e4a8be698ef7f376ff3eaaa7ca25c239dd16
danyygtz <19030013@itcelaya.edu.mx>
2023-12-07 14:55:23 -0600

    app final

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,64 @@
+import 'package:lottie/lottie.dart';
+import 'package:topicos/screens/card_language.dart';
+import 'package:flutter/material.dart';
+import 'package:concentric_transition/concentric_transition.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  OnboardingScreen({super.key});
+
+  final data = [
+    CardLanguageData(
+      title: "Favorite Programming Languages",
+      subtitle: "Backend Language",
+      image: const Image(image: NetworkImage("https://i.giphy.com/media/Dh5q0sShxgp13DwrvG/giphy.webp")),
+      backgroundColor: const Color(0xFF285481),
+      titleColor: const Color.fromARGB(255, 0, 0, 0),
+      subtitleColor: Colors.white,
+      // background: LottieBuilder.asset("assets/desktop_center.json"),
+      alignment: Alignment.bottomCenter,
+    ),
+    CardLanguageData(
+      title: "Go",
+      subtitle: "Backend Language",
+      image: Image.asset("assets/esfera.png"),
+      backgroundColor: const Color(0xFF027D9C),
+      titleColor: Colors.white,
+      subtitleColor: Colors.white,
+      // background: LottieBuilder.asset("assets/buble_right_top.json"),
+      alignment: Alignment.topCenter
+    ),
+    CardLanguageData(
+      title: "Dart",
+      subtitle: "Multi-paradigm Language",
+      image: Image.asset("assets/esfera.png"),
+      backgroundColor: const Color(0xFF1B2834),
+      titleColor: Colors.white,
+      subtitleColor: Colors.red,
+      // background: LottieBuilder.asset("assets/bubble_left_bottom.json"),
+      alignment: Alignment.bottomCenter,
+    ),
+    CardLanguageData(
+      title: "Swift",
+      subtitle: "Multi-paradigm Language",
+      image: Image.asset("assets/esfera.png"),
+      backgroundColor: const Color(0xFF233055),
+      titleColor: Colors.white,
+      subtitleColor: Colors.red,
+      // background: LottieBuilder.asset("assets/figures.json"),
+      alignment: Alignment.bottomCenter
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ConcentricPageView(
+        colors: data.map((e) => e.backgroundColor).toList(),
+        itemCount: 4,
+        itemBuilder: (int index) {
+          return CardLanguage(data: data[index]);
+        },
+      ),
+    );
+  }
+}
